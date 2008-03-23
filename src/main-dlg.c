f57233d5c5cc4ba63cbf00a87085f303911adb79
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2008-03-23 02:47:56 +0000

    Add a desktop-independent gtk+ theme switcher for LXDE. Add LXDE icon for lxpanel in lxde-common.

---
+++ src/main-dlg.c
@@ -0,0 +1,18 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "main-dlg.h"
+#include "main-dlg-ui.h"
+#include "glade-support.h"
+
+
+void
+on_apply_clicked                       (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ src/main-dlg.h
@@ -0,0 +1,6 @@
+#include <gtk/gtk.h>
+
+
+void
+on_apply_clicked                       (GtkButton       *button,
+                                        gpointer         user_data);
