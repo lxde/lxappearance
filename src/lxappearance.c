lxde/lxappearance

commit b60cb9ecc0e45faf790da26eb8b1df09b7d32522

Only do X11-specific stuff on X11

src/lxappearance.c
@@ -117,16 +117,20 @@ static gboolean check_lxde_dbus()
 
 static void check_lxsession()
 {
-    lxsession_atom = XInternAtom( GDK_DISPLAY_XDISPLAY(gdk_display_get_default()), "_LXSESSION", True );
-    if( lxsession_atom != None )
+    GdkDisplay *display = gdk_display_get_default();
+    if (GDK_IS_X11_DISPLAY(display))
     {
-        XGrabServer( GDK_DISPLAY_XDISPLAY(gdk_display_get_default()) );
-        if( XGetSelectionOwner( GDK_DISPLAY_XDISPLAY(gdk_display_get_default()), lxsession_atom ) )
+        lxsession_atom = XInternAtom( GDK_DISPLAY_XDISPLAY(display), "_LXSESSION", True );
+        if( lxsession_atom != None )
         {
-            app.use_lxsession = TRUE;
-            lxsession_name = g_getenv("DESKTOP_SESSION");
+            XGrabServer( GDK_DISPLAY_XDISPLAY(display) );
+            if( XGetSelectionOwner( GDK_DISPLAY_XDISPLAY(display), lxsession_atom ) )
+            {
+                app.use_lxsession = TRUE;
+                lxsession_name = g_getenv("DESKTOP_SESSION");
+            }
+            XUngrabServer( GDK_DISPLAY_XDISPLAY(display) );
         }
-        XUngrabServer( GDK_DISPLAY_XDISPLAY(gdk_display_get_default()) );
     }
 
     /* Check Lxsession also with dbus */

commit e05c7e59931c18ffdbc179979fb47ffec156f6c3

Fix compilation with GTK+ 2
This fixes a regression introduced in
commit b60cb9ecc0e45faf790da26eb8b1df09b7d32522,
github pull request #1.

src/lxappearance.c
@@ -118,7 +118,9 @@ static gboolean check_lxde_dbus()
 static void check_lxsession()
 {
     GdkDisplay *display = gdk_display_get_default();
+#if GTK_CHECK_VERSION(3, 0, 0)
     if (GDK_IS_X11_DISPLAY(display))
+#endif
     {
         lxsession_atom = XInternAtom( GDK_DISPLAY_XDISPLAY(display), "_LXSESSION", True );
         if( lxsession_atom != None )